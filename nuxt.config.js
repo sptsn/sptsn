f9fea28e5a21034c5665bbf7eaeb74e102339b6d
Alexander Spitsyn <sanspits@yandex.ru>
2023-06-08 23:26:08 +0300

    fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'sptsn',
+    title: 'Alexander Spitsyn | Software Engineer',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: "Alexander Spitsyn's site" },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
--- pages/index.vue
+++ pages/index.vue
@@ -23,18 +23,5 @@ import siteMetaInfo from "@/data/sitemetainfo";
 export default {
   name: "IndexPage",
   components: { AuthorCard, AuthorIntro, Expertise, TimeLine },
-  head: {
-    title: siteMetaInfo.title,
-    meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      {
-        hid: "description",
-        name: "description",
-        content: siteMetaInfo.description,
-      },
-    ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
-  },
 }
 </script>

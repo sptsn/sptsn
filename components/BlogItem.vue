68e3e87670e44270aee92f8e88c3ef72a726d754
Alexander Spitsyn <sanspits@yandex.ru>
2023-06-17 14:54:20 +0300

    blog item markup

--- components/BlogItem.vue
+++ components/BlogItem.vue
@@ -1,26 +1,28 @@
 <template>
-  <article class="relative flex flex-col max-w-3xl lg:ml-auto xl:max-w-none">
-    <span class="mb-4">
-      <h3 class="mb-4 text-4xl text-slate-700 tracking-tight font-bold dark:text-slate-200">
-        <NuxtLink :to="{ name: 'blog-slug', params: { slug: source.slug } }">
-          {{ source.title }}
-        </NuxtLink>
-      </h3>
-      <time class="bg-indigo-500 text-white py-1 px-2 rounded-md" datetime="2022-02-24T12:00:00.000Z">
-        {{ formatDate(source.date) }}
-      </time>
-    </span>
+  <NuxtLink :to="{ name: 'blog-slug', params: { slug: source.slug } }">
+    <article class="relative flex flex-col max-w-3xl lg:ml-auto xl:max-w-none p-5 mb-5 rounded-md">
+      <div class="flex flex-row">
+        <div class="basis-4/5">
+          <h3 class="mb-4 text-4xl text-slate-700 tracking-tight font-bold dark:text-slate-200">
+              {{ source.title }}
+          </h3>
+        </div>
+        <div class="basis-1/5 h-8 bg-indigo-500 text-white py-1 px-2 rounded-md font-semibold text-center">
+          {{ formatDate(source.date) }}
+        </div>
+      </div>
 
-    <div class="mb-6 prose prose-slate dark:prose-dark">
-      <template v-if="this.highlight">
-        <pre class="language-html" v-html="highlight.content[0]"></pre>
-      </template>
+      <div class="prose prose-slate dark:prose-dark">
+        <template v-if="this.highlight">
+          <pre class="language-html" v-html="highlight.content[0]"></pre>
+        </template>
 
-      <p v-else>
-        {{ source.description }}
-      </p>
-    </div>
-  </article>
+        <p class="text-gray-500" v-else>
+          {{ source.description }}
+        </p>
+      </div>
+    </article>
+  </NuxtLink>
 </template>
 
 <script>
@@ -40,3 +42,10 @@
     },
   }
 </script>
+
+<style>
+article:hover {
+  background-color: rgb(241 245 249);
+  cursor: pointer;
+}
+</style>

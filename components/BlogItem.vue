732754760bdf20922f7f23154024706c89c7d9e6
Alexander Spitsyn <sanspits@yandex.ru>
2023-06-23 23:19:44 +0300

    update markup

--- components/BlogItem.vue
+++ components/BlogItem.vue
@@ -17,7 +17,9 @@
       </p>
 
       <div v-if="source.tags" class="flex flex-row">
-        <Badge v-for="tag in source.tags" :value="tag" color="indigo" />
+        <span v-for="tag in source.tags" class="bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300">
+          {{ tag }}
+        </span>
       </div>
     </div>
   </NuxtLink>
--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,35 +1,25 @@
 <template>
-  <div class="p-4 md:w-1/2 md" style="max-width: 544px">
-    <div class="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
-      <div class="p-6">
-        <div class="flex flex-row justify-between items-center">
-          <h2 class="text-2xl font-bold leading-8 tracking-tight mb-3">
-            {{ this.item.title }}
-          </h2>
+  <div class="p-4 md:w-1/2 md">
+    <div class="project-card h-full p-4 overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
+      <div class="flex flex-row justify-between">
+        <h2 class="text-2xl font-bold leading-8 tracking-tight mb-3">
+          {{ this.item.title }}
+        </h2>
 
-          <div class="flex flex-row justify-between">
-            <div class="mx-1">
-              <a class="text-sm text-gray-500 transition hover:text-gray-600" target="_blank" rel="noopener noreferrer" :href="this.item.href">
-                <span class="sr-only"></span>
-                <img class="w-6 h-6" src="/external.png"/>
-              </a>
-            </div>
-          </div>
-        </div>
+        <a class="external-link text-sm text-gray-500 transition hover:text-gray-600" target="_blank" rel="noopener noreferrer" :href="this.item.href">
+          <span class="sr-only"></span>
+          <img class="w-6 h-6" src="/external.png"/>
+        </a>
+      </div>
 
-        <p class="prose text-gray-500 max-w-none dark:text-gray-400 mb-3">
-          {{ this.item.description }}
-        </p>
+      <p class="prose text-gray-500 max-w-none dark:text-gray-400 mb-3">
+        {{ this.item.description }}
+      </p>
 
-        <div class="flex flex-row justify-between">
-          <div class="text-gray-400 text-sm font-extralight">
-            <template v-for="(tech, index) in item.tech">
-              <span @click="addTag(tech)" :class="[selectedTags.includes(tech) ? 'bg-blue-400 text-white' : 'bg-blue-100 text-blue-800']" class="text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
-                {{ tech }}
-              </span>
-            </template>
-          </div>
-        </div>
+      <div class="text-gray-400 text-sm font-extralight">
+        <span v-for="(tech, index) in item.tech" @click="addTag(tech)" :class="[selectedTags.includes(tech) ? 'bg-blue-400 text-white' : 'bg-blue-100 text-blue-800']" class="project-card text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+          {{ tech }}
+        </span>
       </div>
     </div>
   </div>
@@ -42,10 +32,18 @@
 </script>
 
 <style>
-  span {
+  a.external-link:hover {
+    filter: brightness(105%);
+  }
+
+  div.project-card:hover {
+    background-color: rgb(241 245 249);
+  }
+
+  span.project-card {
     cursor: pointer;
   }
-  span:hover {
+  span.project-card:hover {
     filter: brightness(105%);
   }
 </style>

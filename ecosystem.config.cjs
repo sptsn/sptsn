309443858c0023a83af11601538d8c384b75ddc8
Alexander Spitsyn <sanspits@yandex.ru>
2024-02-01 17:21:26 +0300

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,11 +3,6 @@ module.exports = {
     {
       name: 'sptsn',
       port: '8000',
-      env: {
-              PORT: 8000,
-NITRO_PORT: 8000,
-              NUXT_PORT: 8000,
-      },
       exec_mode: 'fork',
       instances: 'max',
       script: './.output/server/index.mjs',
